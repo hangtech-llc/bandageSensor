57f6fb87a91c7c796b5bf1fa91fdd7cfebd2bc63
hangtech <53499597+hangtech@users.noreply.github.com>
2020-06-01 07:33:35 -0400

    Initial storyboard with nav controller

---
+++ headSensor/headSensor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  headSensor
+//
+//  Created by Hall, Sean M. on 6/1/20.
+//  Copyright © 2020 Hall, Sean M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
