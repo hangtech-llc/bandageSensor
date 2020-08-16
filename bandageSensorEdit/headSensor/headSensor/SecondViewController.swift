8081d1d690b494a3645e9d9f3945ea197dd4dba9
hangtech <53499597+hangtech@users.noreply.github.com>
2020-08-16 02:34:19 -0400

    Molly (#7)
    * Add files via upload
    
    I'm still working on the navigation behind everything but this is what I've done so far. The blank screen is for more questions that the person using will want to ask the patients.
    
    * Add files via upload
    
    This is the updated storyboard with navigation. I am still working on individual parts but you can navigate throughout the app now
    
    * Add files via upload
    
    * Add files via upload
    
    Co-authored-by: mollylynch <65363138+mollylynch@users.noreply.github.com>

---
+++ bandageSensorEdit/headSensor/headSensor/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  headSensor
+//
+//  Created by Hall, Sean M. on 6/1/20.
+//  Copyright © 2020 Hall, Sean M. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
